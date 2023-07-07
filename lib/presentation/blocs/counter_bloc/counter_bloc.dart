caee3a04999fbe35b686b0879f6d44fc4d23d635
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-07 13:38:13 -0400

    Simplificar el handler del BLoC

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -7,11 +7,14 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
     // Emisión del nuevo evento
-    on<CounterIncreased>((event, emit) {
-      emit(state.copyWith(
-        counter: state.counter + event.value,
-        transactionCounter: state.transactionCounter + 1,
-      ));
-    });
+    on<CounterIncreased>(_onCounterIncreased);
+  }
+
+  // Simplificar el handler del BLoC
+  void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
+    emit(state.copyWith(
+      counter: state.counter + event.value,
+      transactionCounter: state.transactionCounter + 1,
+    ));
   }
 }
